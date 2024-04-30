31423f8b487954dbec13b249dab44a08fbd98385
Kasim kazmi <kasimkazmi27@gmail.com>
2024-04-30 19:56:34 -0400

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
 import { useState } from "react";
-import { TodoForm, TodoList } from "./components/";
+import { TodoForm ,TodoList} from "./components/";
 import { TodoPrvoider } from "./context";
 
+
+
 export default function App() {
-  const [todos, setTodos] = useState([]);
-  console.log(todos, "todos");
-  const addTodo = (todo) => {
-    setTodos((prev) => [
-      ...prev,
-      { id: Date.now(), ...todo, completed: false },
-    ]);
-  };
+const [todos, setTodos] = useState([])
+console.log(todos,"todos");
+const addTodo=(todo)=>
+{
+setTodos ((prev)=> [...prev, {id:Date.now(), ...todo, completed :false} ])
+
+}
   return (
-    <TodoPrvoider value={{ todos, addTodo }}>
-      <div className=" bg-gray-900 ">
-        <div
-          className=" w-full h-screen  max-w-xl
-       mx-auto text-white"
-        >
-          <TodoForm />
-          <TodoList />
-        </div>
+
+    <TodoPrvoider  value={{todos, addTodo}}>
+    <div className=" bg-gray-900 ">
+      <div className=" w-full h-screen  max-w-xl
+       mx-auto text-white">
+        <TodoForm />
+        <TodoList />
       </div>
-    </TodoPrvoider>
+    </div>
+  </TodoPrvoider>
+
   );
+
 }
